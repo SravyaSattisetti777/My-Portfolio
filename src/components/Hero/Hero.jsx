d0be39ac3743a47914069529a0696dcaa4718e43
Sravya Sattisetti <144662582+SravyaSattisetti777@users.noreply.github.com>
2025-03-10 14:58:10 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,12 +11,12 @@ export const Hero = () => {
         <p className={styles.description}>
         Hi!!, I’m Sravya Sattisetti, an enthusiastic B.Tech student passionate about Full Stack Web Development, AI & ML, and Software Development. I thrive on transforming ideas into real-world solutions by applying my knowledge of Computer Science and Emerging Technologies. With a strong focus on practical learning, I continuously explore new tools, frameworks, and technologies to build impactful projects. My interest in open-source contributions and research development drives me to create innovative solutions that can solve real-world challenges. I’m always eager to collaborate, innovate, and contribute to projects that make a difference. 🚀💡
         </p>
-        <a href="https://drive.google.com/file/d/1uFd5oJw7uatbsGuvTxF8li-rut4NPVDT/view?usp=sharing" className={styles.contactBtn}>
+        <a href="https://drive.google.com/file/d/1h0BIvdyRz61hCVJUa1EvwDrrEBpUd53q/view?usp=sharing" className={styles.contactBtn}>
           My Resume
         </a>
       </div>
       <img
-        src={getImageUrl("hero/profileImage.png")}
+        src={getImageUrl("hero/SravyaPic (1).jpg")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
