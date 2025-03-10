ab4750d4fef06ed7fb5e69be79d87c0cf01e8c0d
Sravya Sattisetti <144662582+SravyaSattisetti777@users.noreply.github.com>
2025-03-10 15:00:33 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, This is Sravya</h1>
         <p className={styles.description}>
-        Hi!!, I’m Sravya Sattisetti, an enthusiastic B.Tech student passionate about Full Stack Web Development, AI & ML, and Software Development. I thrive on transforming ideas into real-world solutions by applying my knowledge of Computer Science and Emerging Technologies. With a strong focus on practical learning, I continuously explore new tools, frameworks, and technologies to build impactful projects. My interest in open-source contributions and research development drives me to create innovative solutions that can solve real-world challenges. I’m always eager to collaborate, innovate, and contribute to projects that make a difference. 🚀💡
+        I’m Sravya Sattisetti, an enthusiastic B.Tech student passionate about Full Stack Web Development, AI & ML, and Software Development. I thrive on transforming ideas into real-world solutions by applying my knowledge of Computer Science and Emerging Technologies. With a strong focus on practical learning, I continuously explore new tools, frameworks, and technologies to build impactful projects. My interest in open-source contributions and research development drives me to create innovative solutions that can solve real-world challenges. I’m always eager to collaborate, innovate, and contribute to projects that make a difference. 🚀💡
         </p>
         <a href="https://drive.google.com/file/d/1h0BIvdyRz61hCVJUa1EvwDrrEBpUd53q/view?usp=sharing" className={styles.contactBtn}>
           My Resume
